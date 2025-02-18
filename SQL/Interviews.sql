3a33a3a56e459bec1f995bbef7e0c04e279f711a
mohsen bahaloo <97626744+mbahaloo2022@users.noreply.github.com>
2025-02-18 20:57:52 +0100

    Update Interviews.sql

--- SQL/Interviews.sql
+++ SQL/Interviews.sql
@@ -8,5 +8,5 @@ JOIN Colleges AS CL ON CT.contest_id = CL.contest_id
 JOIN Challenges AS CH ON CH.college_id = CL.college_id
 left JOIN ( SELECT CHALLENGE_ID, SUM(total_views) AS TV, SUM(total_unique_views) AS TUV FROM View_Stats GROUP BY CHALLENGE_ID )  VS ON CH.challenge_id = VS.challenge_id
 left JOIN (SELECT CHALLENGE_ID, SUM(TOTAL_SUBMISSIONS) AS TS, SUM(TOTAL_ACCEPTED_SUBMISSIONS) AS TAS FROM Submission_Stats GROUP BY CHALLENGE_ID ) AS SS ON CH.challenge_id = SS.challenge_id
-GROUP BY CT.contest_id
+GROUP BY     CT.contest_id, CT.hacker_id, CT.name
 ORDER BY CT.contest_id
